0bf1e681bbcbc67a9b6c9e7e4559484f374b6fb2
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-08-11 15:20:12 -0300

    commit bia

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-// DECLARAÇÃO DE VARIÁVEIS
-let operacao = prompt ("Informe a operação desejada: ")
-let numero1 = Number (prompt("Informe o primeiro número: "))
-let numero2 = Number (prompt("Informe o segundo número: "))
+// Declaração das variáveis
+let operacao = prompt("Informe a operação desejada: ");
+let numero1 = Number(prompt("Informe o primeiro número: "));
+let numero2 = Number(prompt("Informe o segundo número: "));
+let resultado;
 
-//VERIFICAR A OPERAÇÃO MATEMÁTICA SELECIONADA
-if (operacao == "+") {
-    let resultado = numero1 + numero2
-    console.log("O resultado da soma é " + resultado + ".")
+// Verificar a operação matemática selecionada
+if (operacao === "+") {
+    resultado = numero1 + numero2;
+} else if (operacao === "-") {
+    resultado = numero1 - numero2;
+} else if (operacao === "*") {
+    resultado = numero1 * numero2;
+} else if (operacao === "/") {
+    resultado = numero1 / numero2;
+} else {
+    resultado = "Operação inválida.";
 }
 
-else if (operacao == "-") {
-    let resultado = numero1 - numero2
-    console.log("O resultado da subtração é " + resultado + ".")
-}
-
-else if(operacao == "*") {
-    let resultado = numero1 * numero2
-    console.log("O resultado da multiplicação é " + resultado + ".")
-}
-
-else if(operacao == "/") {
-    let resultado = numero1 / numero2
-    console.log("O resultado da divisão é " + resultado + ".")
-}
+// Exibir o resultado
+document.write("<p>O resultado da operação é: " + resultado + "</p>");
